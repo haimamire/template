d13f825807408867039af942a5ce445090686334
haimamire <ivanmatusevich16@gmail.com>
2026-04-16 18:02:13 -0300

    Update webpack to use ESM rather than CJS

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,10 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+import { merge } from "webpack-merge";
+import common from "./webpack.common.js";
 
-module.exports = merge(common, {
-  mode: 'development',
-  devtool: 'eval-source-map',
+export default merge(common, {
+  mode: "development",
+  devtool: "eval-source-map",
   devServer: {
-    watchFiles: ['./src/template.html'],
+    watchFiles: ["./src/template.html"],
   },
 });
